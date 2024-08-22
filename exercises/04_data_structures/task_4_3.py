91355a95ea12fa7896af523518fb5cbe4bde4959
Omar Tulashvili <omartulashvili.ge@gmail.com>
2024-08-22 23:10:34 +0400

    done and check 4.1-4.3 exercises

--- exercises/04_data_structures/task_4_3.py
+++ exercises/04_data_structures/task_4_3.py
@@ -22,3 +22,11 @@
 """
 
 config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+split_string = config.split() # ['switchport', 'trunk', 'allowed', 'vlan', '1,3,10,20,30,100']
+vlans = split_string[-1].split(",")
+print(vlans)
+
+### OR ###
+
+result = config.split()[-1].split(",")
+print(result)
